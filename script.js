c8f20226b74e7e2643a09b2871e20137837eb774
Juginhikipaja <jukka.palm@hotmail.fi>
2025-11-17 06:37:39 +0200

    Sectioneita lisätty

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
 function vaihdaTeema(teema) {
     document.body.className = teema;
+
+
+    const sections = document.querySelectorAll('main.main-content section');
+    sections.forEach(sec => sec.style.display = 'none');
+
+    switch(teema) {
+        case 'theme-index':
+            document.getElementById('section-index').style.display = 'block';
+            break;
+        case 'theme-taidot':
+            document.getElementById('section-taidot').style.display = 'block';
+            break;
+        case 'theme-portfolio':
+            document.getElementById('section-portfolio').style.display = 'block';
+            break;
+    }
 }
